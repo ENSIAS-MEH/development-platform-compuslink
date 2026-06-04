b07d4f4581a643ebcd38d606dbef6e1b7eb98be8
kaddouriayoub <kaddouri.ayoub1308@gmail.com>
2026-06-04 03:15:28 +0100

    feat: add docker-compose and database init script

---
+++ compuslink-microservices/init-db.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE compuslink_users;
+CREATE DATABASE compuslink_marketplace;
+CREATE DATABASE compuslink_colocation;
+CREATE DATABASE compuslink_offers;
+CREATE DATABASE compuslink_events;
+CREATE DATABASE compuslink_messaging;
+CREATE DATABASE compuslink_common;
